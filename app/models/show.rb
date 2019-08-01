6398a0a93e183bd93ce3effaced0a95b4e322ce8
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-08-01 02:30:59 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def Show::most_popular_show
-    Show.where("highest_rating")
+    Show.order(rating: :desc).first
   end
 
   def Show::lowest_rating
@@ -13,6 +13,7 @@ class Show < ActiveRecord::Base
   end
 
   def Show::least_popular_show
+    Show.order(rating: :asc).first
   end
 
   def Show::ratings_sum
